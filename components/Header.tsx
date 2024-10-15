193ffc8aad768a093ed374d99ce7c6e89ed28411
Joseito Fernandes <joseito.terence@gmail.com>
2024-10-15 21:40:21 +0100

    WIP: be22d93 init: portfolio

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,13 @@ import { FloatingNav } from "./ui/floating-navbar";
 export default function Header() {
   return (
     <header className="py-6">
-      <h1 className="text-4xl font-bold">Your Name</h1>
+      <h1 className="text-4xl font-bold">Joseito</h1>
       <p className="text-xl">Software Developer</p>
 
-      <FloatingNav navItems={navItems} />
+      <FloatingNav 
+        navItems={navItems}
+        className="isolate bg-white/20 shadow-lg ring-1 ring-black/5"
+      />
     </header>
   );
 }
@@ -15,16 +18,13 @@ const navItems = [
   {
     name: "Home",
     link: "/",
-    // icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Projects",
+    link: "/projects",
   },
   {
     name: "About",
     link: "/about",
-    // icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
-  },
-  {
-    name: "Contact",
-    link: "/contact",
-    // icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
   },
 ];
