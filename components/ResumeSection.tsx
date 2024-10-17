d60c049e8c5701c89f0894f58fa8d326d3fd34df
Joseito Fernandes <joseito.terence@gmail.com>
2024-10-17 22:14:21 +0100

    attach resume for download

--- components/ResumeSection.tsx
+++ components/ResumeSection.tsx
@@ -14,12 +14,12 @@ export default function ResumeSection() {
 
         <div className='flex-1 flex justify-center items-center'>
           <FadeUp delay={0.5}>
-            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <a href="/resume.pdf" download className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-lg font-medium text-white backdrop-blur-3xl">
                 Download Resume
               </span>
-            </button>
+            </a>
           </FadeUp>
         </div>
       </div>
