1ac55b7d5f1f71ba43067187bf2713f681b7398e
Joseito Fernandes <joseito.terence@gmail.com>
2024-10-18 20:14:15 +0100

    Refactor animation timing in card hover effect

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -8,8 +8,8 @@ const containerVariants = {
   visible: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.3,
-      delayChildren: 0.3,
+      staggerChildren: 0.2,
+      delayChildren: 0.1,
       type: 'spring',
       stiffness: 400,
       damping: 40,
@@ -23,7 +23,7 @@ const itemVariants = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.5,
+      duration: 0.3,
       ease: 'easeInOut'
     },
   },
