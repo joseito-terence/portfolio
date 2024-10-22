d71de3afc7f645b671baa3505118ec8745161bb4
Joseito Fernandes <joseito.terence@gmail.com>
2024-10-22 10:37:30 +0100

    smooth scroll

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -72,10 +72,10 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-        <button className="border text-sm font-medium relative border-white/[0.2] text-white px-4 py-2 rounded-full">
+        <Link href='#contact' className="border text-sm font-medium relative border-white/[0.2] text-white px-4 py-2 rounded-full">
           <span>Contact</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+        </Link>
       </motion.div>
     </AnimatePresence>
   );
