9368536e6296cd34278c643b069770f9e386ca64
Joseito Fernandes <joseito.terence@gmail.com>
2024-10-22 12:47:49 +0100

    footer rewamp

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 import {
   IconBrandGithub,
   IconBrandLinkedin,
   IconMail,
   IconPhone,
-  IconSend,
+  IconCopy,
 } from "@tabler/icons-react";
+import { TextHoverEffect } from "./ui/text-hover-effect";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+import { FadeUp } from "./ui/fade-up";
 
 export const socials = [
   { name: 'Github', link: 'https://github.com/joseito-terence', Icon: IconBrandGithub },
@@ -16,104 +17,83 @@ export const socials = [
 ];
 
 export default function Contact() {
-  const [isSending, setIsSending] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref)
+  const [shown, setShown] = useState(false)
+
+  useEffect(() => {
+    if (isInView && !shown) {
+      setShown(true)
+    }
+  }, [isInView])
+
   return (
     <footer
+      ref={ref}
       id="contact"
-      className="bg-[#273449] flex justify-center items-center overflow-hidden mt-10"
+      className="h-[40rem] w-full rounded-md bg-gradient-to-t from-primary from-80% via-primary/60 to-transparent relative flex flex-col items-center justify-center antialiased"
     >
-      <div
-        className="flex flex-col md:flex-row w-full md:w-[700px] md:m-3 bg-primary md:rounded-xl"
-      >
-        <div
-          className="w-full md:w-[270px] bg-secondary-content text-inherit md:rounded-xl p-5 flex flex-col justify-between"
-        >
-          <div>
-            <h1 className="text-3xl font-bold">Contact</h1>
-            <p className="text-xl font-light mb-9 tracking-wide">
-              Any questions or remarks? Just write me a message
-            </p>
-          </div>
-
-          <div>
-            <a href="mailto:joseito.terence@gmail.com" className="flex items-center gap-1 cursor-pointer">
-              <IconMail size={32} />
-              <span>joseito.terence@gmail.com</span>
-            </a>
-
-            <a href="tel:+447867633529" className="flex items-center gap-1 cursor-pointer">
-              <IconPhone size={32} />
-              <span>+44 7867 633529</span>
-            </a>
-          </div>
+      <div className="h-[50rem] w-full bg-dot-white/[0.2] relative flex flex-col items-center justify-center">
+        <div className="w-full z-30">
+          {shown &&
+            <TextHoverEffect text="CONTACT" />
+          }
         </div>
-        <div className="flex-1 p-4">
-          <form className="flex flex-col gap-4">
-            <LabelInputContainer>
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" type="text" />
-            </LabelInputContainer>
 
-            <LabelInputContainer>
-              <Label htmlFor="message">Message</Label>
-              <Input as='textarea' id="message" className="h-32" />
-            </LabelInputContainer>
+        <div className="flex flex-row flex-wrap justify-center gap-10 md:-mt-40 z-40">
+          <FadeUp duration={1.5}>
+            <Card
+              text="joseito.terence@gmail.com"
+              link="mailto:joseito.terence@gmail.com"
+              Icon={IconMail}
+            />
+          </FadeUp>
 
-            <button
-              type="button"
-              className="
-              flex flex-row items-center justify-center gap-2
-              relative
-              px-12
-              bg-gradient-to-br group/btn from-zinc-900 to-zinc-900 bg-zinc-800 text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-
-              // className="btn mt-7  group  {!isSending && 'pr-[40px]'}"
-            >
-              {isSending ? (
-                <>
-                  Sending
-                  <span className="loading loading-spinner" />
-                </>
-              ) : (
-                <>
-                  Send Message
-                </>
-              )}
-              <div
-                className="w-[25px] h-[25px]
-              
-							transition-all group-hover/btn:rotate-12 {isSending && 'plane'}"
-              >
-                <IconSend />
-              </div>
-              <BottomGradient />
-            </button>
-          </form>
+          <FadeUp duration={1.5} delay={0.5}>
+            <Card
+              text="+44 7867 633529"
+              link="tel:+447867633529"
+              Icon={IconPhone}
+            />
+          </FadeUp>
         </div>
       </div>
     </footer>
   );
 }
 
-const LabelInputContainer = ({
-  children,
-  className,
+export function Card({
+  text,
+  link,
+  Icon,
 }: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
+  text: string;
+  link: string;
+  Icon?: typeof IconMail;
+}) {
   return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
+    <div>
+      <div className="w-full relative min-w-[300px]">
+        <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+        <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <div className="w-full flex items-center justify-between mb-4">
+            <Icon size={30} />
+
+            <button className="aspect-square border px-2 py-1 rounded-lg  border-gray-500 text-gray-300">
+              <IconCopy size={20} />
+            </button>
+          </div>
+
+          <h1 className="w-full font-bold text-xl text-center text-white mb-4 relative z-50">
+            <a href={link}>
+              {text}
+            </a>
+          </h1>
+
+
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
-  );
-};
