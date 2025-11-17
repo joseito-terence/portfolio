fd1ff4b9cdc66e71ab7539453e239ba5f0950127
Joseito Fernandes <74445627+joseito-terence@users.noreply.github.com>
2025-11-17 11:54:20 +0000

    Update experience in metadata description

--- app/constants.ts
+++ app/constants.ts
@@ -14,7 +14,7 @@ import TamaguiIcon from "@/icons/tamagui";
 
 export const metadata: Metadata = {
   title: "Joseito Fernandes",
-  description: "👋 Hello! I'm a dedicated Software Developer fueled by an unwavering commitment to perpetual learning and progress. With 3 years of professional experience, I've crafted applications using an extensive tech stack, including React.js, React Native, TypeScript, Express.js, MongoDB, and more. My unwavering dedication lies in continually refining my expertise and competencies.",
+  description: "👋 Hello! I'm a dedicated Software Developer fueled by an unwavering commitment to perpetual learning and progress. With 4 years of professional experience, I've crafted applications using an extensive tech stack, including React.js, React Native, TypeScript, Express.js, MongoDB, and more. My unwavering dedication lies in continually refining my expertise and competencies.",
 };
 
 export const skills = [
